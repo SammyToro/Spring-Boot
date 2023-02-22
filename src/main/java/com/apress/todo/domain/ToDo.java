aeec3b8409a94095625c1faa0c28a13e15fa1070
SammyToro <samolonzy@rocketmail>
2023-02-22 08:57:13 +0000

    todo-jpa

---
+++ src/main/java/com/apress/todo/domain/ToDo.java
@@ -0,0 +1,56 @@
+package com.apress.todo.domain;
+
+import java.time.LocalDateTime;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class ToDo {
+
+    @NotNull
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid",strategy = "uuid")
+    private String id;
+
+    @NotNull
+    @NotBlank
+    private String description;
+
+    @Column(insertable = true, updatable = false)
+    private LocalDateTime created;
+    private LocalDateTime modified;
+    private boolean completed;
+
+
+    public ToDo(String description){
+        this();
+        this.description = description;
+    }
+
+    @PrePersist
+    void onCreate(){
+        this.setCreated(LocalDateTime.now());
+        this.setModified(LocalDateTime.now());
+    }
+
+    @PreUpdate
+    void onUpdate(){
+        this.setModified(LocalDateTime.now());
+    }
+    
+}
