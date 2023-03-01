b01a263650989771f2ac5a1ad7ab16ad73a25d6c
SammyToro <samolonzy@rocketmail>
2023-03-01 09:24:11 +0000

    reactive fluix type

---
+++ src/main/java/com/apress/reactive/fluxexample/FluxExampleApplication.java
@@ -0,0 +1,13 @@
+package com.apress.reactive.fluxexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FluxExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apress/reactive/fluxexample/FluxExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apress.reactive.fluxexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluxExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
