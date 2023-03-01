6bc2f07c3567e3dca429403f86c3964140c6c6e8
SammyToro <samolonzy@rocketmail>
2023-03-01 09:25:13 +0000

    reactive mono type

---
+++ src/main/java/com/apress/reactor/example/domain/ToDo.java
@@ -0,0 +1,26 @@
+package com.apress.reactor.example.domain;
+
+import java.time.LocalDateTime;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ToDo {
+    
+    private String id;
+    private String description;
+    private LocalDateTime created;
+    private LocalDateTime modified;
+    private boolean completed;
+
+    public ToDo(String description){
+        this.description = description;
+    }
+
+    public ToDo(String description,boolean completed){
+        this.description = description;
+        this.completed = completed;
+    }
+}
