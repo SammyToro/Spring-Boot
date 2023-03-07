423f240121f9b5a7e1ddb44869aed22b6160adc0
SammyToro <samolonzy@rocketmail>
2023-03-07 15:34:33 +0000

    added testing

--- src/test/java/com/apress/todo/TodoRestApplicationTests.java
+++ src/test/java/com/apress/todo/TodoRestApplicationTests.java
@@ -1,10 +1,29 @@
 package com.apress.todo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class TodoRestApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void toDoTest() throws Exception{
+		this.mockMvc
+			.perform(MockMvcRequestBuilders.get("/toDos"))
+			.andExpect(MockMvcResultMatchers.status().isOk())
+			.andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON));
+	}
 
 	@Test
 	void contextLoads() {
