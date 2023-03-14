08bacea7ee7ad69c1a192198c2225dde8bd5f7c3
SammyToro <samolonzy@rocketmail>
2023-03-14 07:35:09 +0000

    Added Test

--- src/test/java/com/apress/todo/TodoJdbcApplicationTests.java
+++ src/test/java/com/apress/todo/TodoJdbcApplicationTests.java
@@ -1,13 +1,36 @@
 package com.apress.todo;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
-@SpringBootTest
+import com.apress.todo.domain.ToDo;
+import com.apress.todo.repository.CommonRepository;
+import com.apress.todo.repository.ToDoRepository;
+
+@JdbcTest
 class TodoJdbcApplicationTests {
+
+	@Autowired
+	private NamedParameterJdbcTemplate jdbcTemplate;
+
+	private CommonRepository<ToDo> repository;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void todoJdbcTest(){
+		ToDo toDo = new ToDo("Read a Book");
+
+		this.repository = new ToDoRepository(jdbcTemplate);
+		this.repository.save(toDo);
+
+		ToDo result = this.repository.findById(toDo.getId());
+		Assertions.assertThat(result.getId()).isEqualTo(toDo.getId());
 	}
 
 }
